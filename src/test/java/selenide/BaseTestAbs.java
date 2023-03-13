7422849a652fc733524e125edcc874067d718957
Aleksander Shatalov <abshatalov87@gmail.com>
2023-03-13 18:44:57 +0300

    allure error

--- src/test/java/selenide/BaseTestAbs.java
+++ src/test/java/selenide/BaseTestAbs.java
@@ -2,20 +2,23 @@ package selenide;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import java.util.Locale;
 
 
 public class BaseTestAbs {
 
 
-  @BeforeEach
-  public void setUp() {
+  @BeforeAll
+  public static void setUp() {
     Configuration.baseUrl = System.getProperty("base_url").trim().toLowerCase(Locale.ROOT);
     Configuration.browser = System.getProperty("browser").trim().toLowerCase(Locale.ROOT);
     Configuration.browserSize = "1920x1080";
-    Configuration.headless = true;
+    Configuration.headless = false;
+    SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
   }
 
   @AfterEach
